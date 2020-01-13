cdc93fe29bde454eed4458f820a1466ad447dfc8
Jack Overby <jroverby92@gmail.com>
2020-01-13 22:27:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,3 +6,28 @@ def map_to_negativize(array)
   end
   array
 end
+
+def map_to_no_change(array)
+  
+end
+
+def map_to_double(array)
+  
+end
+
+def map_to_square(array)
+  
+end
+
+def reduce_to_total(array)
+  
+end
+
+def reduce_to_all_true(array)
+  
+end
+
+def reduce_to_any_true(array)
+  
+end
+
