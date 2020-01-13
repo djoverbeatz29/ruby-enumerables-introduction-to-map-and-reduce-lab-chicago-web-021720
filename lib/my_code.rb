78233f5f60d0a09df50f850e3c1912fa0d025819
Jack Overby <jroverby92@gmail.com>
2020-01-13 22:30:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,15 +1,15 @@
 # My Code here....
 
 def map_to_negativize(array)
-  map
+  array.map{ |i| -i}
 end
 
 def map_to_no_change(array)
-  
+  array.map{ |i| i}
 end
 
 def map_to_double(array)
-  
+  array.map{ |i| i*2}
 end
 
 def map_to_square(array)
